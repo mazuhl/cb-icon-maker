f4b8c53752d0d6079bf483c62b7d2ba64b3a5fc9
max.mitchell@gmail.com <max.mitchell@gmail.com>
2015-04-15 20:44:05 +0100

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # cp-icon-maker
 Icon maker for MODx Content Blocks Extra
+
+Short shell script to icons similar to those used by [ModMore's Content Blocks plugin](https://www.modmore.com/extras/contentblocks/). Great to quickly generate icons for Fields, Layouts and Templates from thousands of free icon images.
+
+## Usage
+
+1. Clone the repo.
+2. Get some icon files in PNG format from FontAwesome, FlatIcon, etc. > 200px works best.
+3. Drop your icons into /images
+4. Run ./cb-icon-maker.sh
+5. If you want 2x2 icons, add the `--multi` option.
+6. Upload your icons into the `/assets/components/contentblocks/img/icons/`
+
+## Short version
+
+```sh
+$ cd cb-icon-maker
+$ ./cb-icon-maker.sh
+```
+
+Then look at your new icons in images. Remember to run all the output icons through a lossless PNG compressor.
---
+++ cb-icon-maker.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# ------------------------------------------------------------------
+# [mazuhl] cb-icon-maker.sh
+#          Make icons for Content Block field types and templates
+#
+# Step 1: Download icons to images/ from FontAwesome, FlatIcon, etc.
+# Step 2: $ cb-icon-maker.sh [--multi]
+# Step 3: Check your icons and upload them to /assets/components/contentblocks/img/icons/
+# ------------------------------------------------------------------
+
+if [ $1 == "--multi" ]
+  then
+    for f in images/*.png ;
+        do 
+          # make 2x2 icons
+          convert -size 160x160 xc:none images/temp.png;
+          convert "$f" -fill white -colorize 40% "$f";    
+          composite -gravity NorthEast -geometry +22+20 \( "$f" -resize 50x42 \) images/temp.png images/temp.png;
+          composite -gravity NorthWest -geometry +22+20 \( "$f" -resize 50x42 \) images/temp.png images/temp.png;
+          composite -gravity SouthEast -geometry +22+25 \( "$f" -resize 50x42 \) images/temp.png images/temp.png;
+          composite -gravity SouthWest -geometry +22+25 \( "$f" -resize 50x42 \) images/temp.png images/temp.png;
+          composite -gravity  center images/temp.png content-blocks-x2-background-multi.png "$f";
+          rm images/temp.png;
+          cp -p "$f" "images/$(basename $f .png)@2x.png";
+          convert "$f" -resize 80x80 "$f";    
+      done
+else
+  for f in images/*.png ;
+    do 
+      # make individual icons
+      convert "$f" -fill white -colorize 40% "$f";
+      composite -gravity center \( "$f" -resize 100x85 \) content-blocks-x2-background.png "$f";
+      cp -p "$f" "images/$(basename $f .png)@2x.png";
+      convert "$f" -resize 80x80 "$f";
+  done
+fi
+
+
+# for f in images/*.png ;
+#   do 
+#     convert -size 160x160 xc:none images/temp.png;
+#     convert "$f" -fill white -colorize 40% "$f";    
+#     composite -gravity NorthEast -geometry +22+20 \( "$f" -resize 50x42 \) images/temp.png images/temp.png;
+#     composite -gravity NorthWest -geometry +22+20 \( "$f" -resize 50x42 \) images/temp.png images/temp.png;
+#     composite -gravity SouthEast -geometry +22+25 \( "$f" -resize 50x42 \) images/temp.png images/temp.png;
+#     composite -gravity SouthWest -geometry +22+25 \( "$f" -resize 50x42 \) images/temp.png images/temp.png;
+#     composite -gravity  center images/temp.png content-blocks-x2-background-multi.png "$f";
+
+#     rm images/temp.png;
+#     cp -p "$f" "images/$(basename $f .png)@2x.png";
+#     convert "$f" -resize 80x80 "$f";    
+# done
